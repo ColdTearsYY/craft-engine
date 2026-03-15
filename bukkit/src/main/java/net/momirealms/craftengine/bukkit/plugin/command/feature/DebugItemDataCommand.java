829ee382bc51c7eb69ef24eac010221a24f6b0ab
XiaoMoMi <littlemomi@qq.com>
2026-03-16 02:01:20 +0800

    优化debug item data实现

--- bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/DebugItemDataCommand.java
+++ bukkit/src/main/java/net/momirealms/craftengine/bukkit/plugin/command/feature/DebugItemDataCommand.java
@@ -1,10 +1,15 @@
 package net.momirealms.craftengine.bukkit.plugin.command.feature;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import net.momirealms.craftengine.bukkit.api.BukkitAdaptor;
 import net.momirealms.craftengine.bukkit.item.BukkitItemManager;
 import net.momirealms.craftengine.bukkit.plugin.command.BukkitCommandFeature;
 import net.momirealms.craftengine.bukkit.plugin.user.BukkitServerPlayer;
-import net.momirealms.craftengine.bukkit.util.ItemStackUtils;
 import net.momirealms.craftengine.bukkit.util.RegistryOps;
 import net.momirealms.craftengine.core.entity.player.InteractionHand;
 import net.momirealms.craftengine.core.item.Item;
@@ -17,12 +22,12 @@ import net.momirealms.craftengine.proxy.minecraft.nbt.CompoundTagProxy;
 import net.momirealms.craftengine.proxy.minecraft.world.item.ItemStackProxy;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.incendo.cloud.Command;
 
 import java.util.*;
 
 public final class DebugItemDataCommand extends BukkitCommandFeature<CommandSender> {
+    private static final TextColor COLOR_TEXT = TextColor.color(0xF5F5F5);
 
     public DebugItemDataCommand(CraftEngineCommandManager<CommandSender> commandManager, CraftEngine plugin) {
         super(commandManager, plugin);
@@ -43,13 +48,12 @@ public final class DebugItemDataCommand extends BukkitCommandFeature<CommandSend
                     if (toClientSide) {
                         itemInHand = BukkitItemManager.instance().s2c(itemInHand, serverPlayer).orElse(itemInHand);
                     }
+
                     Map<String, Object> readableMap = toMap(itemInHand);
-                    List<String> readableList = mapToList(readableMap);
-                    StringJoiner joiner = new StringJoiner("<newline><reset>");
-                    for (String text : readableList) {
-                        joiner.add(text);
-                    }
-                    plugin().senderFactory().wrap(context.sender()).sendMessage(AdventureHelper.miniMessage().deserialize(joiner.toString()));
+                    List<Component> readableComponents = mapToComponentList(readableMap);
+
+                    Component finalMessage = Component.join(JoinConfiguration.separator(Component.newline()), readableComponents);
+                    plugin().senderFactory().wrap(context.sender()).sendMessage(finalMessage);
                 });
     }
 
@@ -70,50 +74,49 @@ public final class DebugItemDataCommand extends BukkitCommandFeature<CommandSend
         }
     }
 
-    private List<String> mapToList(Map<String, Object> readableDataMap) {
-        List<String> list = new ArrayList<>();
-        mapToList(readableDataMap, list, 0, false);
+    private List<Component> mapToComponentList(Map<String, Object> readableDataMap) {
+        List<Component> list = new ArrayList<>();
+        mapToComponentList(readableDataMap, list, 0, false);
         return list;
     }
 
     @SuppressWarnings("unchecked")
-    private void mapToList(Map<String, Object> map, List<String> readableList, int loopTimes, boolean isMapList) {
+    private void mapToComponentList(Map<String, Object> map, List<Component> readableList, int loopTimes, boolean isMapList) {
         boolean first = true;
         for (Map.Entry<String, Object> entry : map.entrySet()) {
             Object nbt = entry.getValue();
+
+            String indent = "  ".repeat(isMapList && first ? Math.max(0, loopTimes - 1) : loopTimes);
+            String prefixString = (isMapList && first) ? indent + "- " : indent;
+            Component prefix = Component.text(prefixString, COLOR_TEXT);
+
+            Component gradientKey = AdventureHelper.miniMessage().deserialize("<gradient:#FFD700:#FFFACD>" + entry.getKey() + "</gradient>");
+
             if (nbt instanceof List<?> list) {
-                if (isMapList && first) {
-                    first = false;
-                    readableList.add("  ".repeat(loopTimes - 1) +
-                            "<#F5F5F5>- <gradient:#FFD700:#FFFACD><hover:show_text:'<yellow>List'>" + entry.getKey() + "</hover></gradient>:</#F5F5F5>");
-                } else {
-                    readableList.add("  ".repeat(loopTimes) +
-                            "<#F5F5F5><gradient:#FFD700:#FFFACD><hover:show_text:'<yellow>List'>" + entry.getKey() + "</hover></gradient>:</#F5F5F5>");
-                }
+                readableList.add(prefix
+                        .append(gradientKey.hoverEvent(HoverEvent.showText(Component.text("List", NamedTextColor.YELLOW))))
+                        .append(Component.text(":", COLOR_TEXT))
+                );
+
                 for (Object value : list) {
                     if (value instanceof Map<?,?> innerDataMap) {
-                        mapToList((Map<String, Object>) innerDataMap, readableList, loopTimes + 2, true);
+                        mapToComponentList((Map<String, Object>) innerDataMap, readableList, loopTimes + 2, true);
                     } else {
-                        if (value instanceof String string) {
-                            readableList.add("  ".repeat(loopTimes + 1)
-                                    + "<#F5F5F5>- <hover:show_text:'<yellow>Copy'><click:suggest_command:'" + string.replace("'", "\\'") + "'>" + value + "</click></hover></#F5F5F5>");
-                        } else {
-                            readableList.add("  ".repeat(loopTimes + 1)
-                                    + "<#F5F5F5>- <hover:show_text:'<yellow>Copy'><click:suggest_command:'" + value + "'>" + value + "</click></hover></#F5F5F5>");
-                        }
+                        String strValue = String.valueOf(value);
+                        readableList.add(Component.text("  ".repeat(loopTimes + 1) + "- ", COLOR_TEXT)
+                                .append(Component.text(strValue, COLOR_TEXT)
+                                        .hoverEvent(HoverEvent.showText(Component.text("Copy", NamedTextColor.YELLOW)))
+                                        .clickEvent(ClickEvent.suggestCommand(strValue)))
+                        );
                     }
                 }
             } else if (nbt instanceof Map<?,?> innerMap) {
-                if (isMapList && first) {
-                    first = false;
-                    readableList.add("  ".repeat(loopTimes - 1) +
-                            "<#F5F5F5>- <gradient:#FFD700:#FFFACD><hover:show_text:'<yellow>Map'>" + entry.getKey() + "</hover></gradient>:</#F5F5F5>");
-                } else {
-                    readableList.add("  ".repeat(loopTimes) +
-                            "<#F5F5F5><gradient:#FFD700:#FFFACD><hover:show_text:'<yellow>Map'>" + entry.getKey() + "</hover></gradient>:");
-                }
-                mapToList((Map<String, Object>) innerMap, readableList, loopTimes + 1, false);
-            } else  {
+                readableList.add(prefix
+                        .append(gradientKey.hoverEvent(HoverEvent.showText(Component.text("Map", NamedTextColor.YELLOW))))
+                        .append(Component.text(":", COLOR_TEXT))
+                );
+                mapToComponentList((Map<String, Object>) innerMap, readableList, loopTimes + 1, false);
+            } else {
                 String value;
                 if (nbt.getClass().isArray()) {
                     value = switch (nbt) {
@@ -132,18 +135,16 @@ public final class DebugItemDataCommand extends BukkitCommandFeature<CommandSend
                     value = nbt.toString();
                 }
 
-                if (isMapList && first) {
-                    first = false;
-                    readableList.add("  ".repeat(loopTimes - 1) +
-                            "<#F5F5F5>- <gradient:#FFD700:#FFFACD><hover:show_text:'<yellow>" + nbt.getClass().getSimpleName() + "'>" + entry.getKey() + "</hover></gradient>" +
-                            ": " +
-                            "<#F5F5F5><hover:show_text:'<yellow>Copy'><click:suggest_command:'" + value.replace("'", "\\'") + "'>" + value + "</click></hover></#F5F5F5>");
-                } else {
-                    readableList.add("  ".repeat(loopTimes) +
-                            "<#F5F5F5><gradient:#FFD700:#FFFACD><hover:show_text:'<yellow>" + nbt.getClass().getSimpleName() + "'>" + entry.getKey() + "</hover></gradient>" +
-                            ": " +
-                            "<hover:show_text:'<yellow>Copy'><click:suggest_command:'" + value.replace("'", "\\'") + "'>" + value + "</click></hover></#F5F5F5>");
-                }
+                readableList.add(prefix
+                        .append(gradientKey.hoverEvent(HoverEvent.showText(Component.text(nbt.getClass().getSimpleName(), NamedTextColor.YELLOW))))
+                        .append(Component.text(": ", COLOR_TEXT))
+                        .append(Component.text(value, COLOR_TEXT)
+                                .hoverEvent(HoverEvent.showText(Component.text("Copy", NamedTextColor.YELLOW)))
+                                .clickEvent(ClickEvent.suggestCommand(value)))
+                );
+            }
+            if (isMapList) {
+                first = false;
             }
         }
     }
